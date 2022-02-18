17c6b6369f912b579e2de0276242065fdf04ec60
Aurelie Gedeon <agedeon540@gmail.com>
2022-02-18 11:03:46 -0500

    connected frontend to api

--- src/components/Tasklist.js
+++ src/components/Tasklist.js
@@ -6,15 +6,21 @@ const fakeTasks = [
   { id: 1, task: "Buy Milk", done: false },
   { id: 2, task: "Eat Chips", done: false },
   { id: 3, task: "Eat Cereal", done: false },
-  { id: 4, task: "Go Ham", done: false },
+  { id: 4, task: "Go Ham", done: true },
   { id: 5, task: "Buy Cheese", done: false },
 ];
 export default function TaskList() {
   const [tasks, setTasks] = useState(fakeTasks);
+  useEffect(() => {
+    fetch("https://much-todo-ag.uc.r.appspot.com/tasks")
+      .then((res) => res.json())
+      .then((data) => setTasks(data))
+      .catch(alert);
+  }, []);
   return (
     <List
       bordered
-      dataSource={fakeTasks}
+      dataSource={tasks}
       renderItem={(item) => <Task item={item} />}
     />
   );
