49bf792dc032ddf53234804140e79b696dd5924c
Aurelie Gedeon <agedeon540@gmail.com>
2022-02-21 16:13:15 -0500

    deployed app

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,7 @@
 import { List } from "antd";
 import { useEffect, useState } from "react";
-export default function Task({ item }) {
+
+export default function Task({ item, setTasks }) {
   const [itemStyle, setItemStyle] = useState({});
   useEffect(() => {
     if (item.done) {
@@ -9,8 +10,23 @@ export default function Task({ item }) {
       setItemStyle({ color: "black", textDecoration: "none" });
     }
   }, [item]);
+
   const handleToggleTaskDone = () => {
     //call API -- patch: `/tasks/${item.id}` send done: !item.done
+    fetch(`https://much-todo-ag.uc.r.appspot.com/tasks/${item.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({ done: !item.done }),
+    })
+      .then(() => {
+        fetch("https://much-todo-ag.uc.r.appspot.com/tasks")
+          .then((res) => res.json())
+          .then((data) => setTasks(data));
+      })
+      .catch(alert);
     //THEN: fetch tasks
     //THEN: setTasks(data)
   };
